6348b3789d90a2eff72693d2b7647164c81b5412
Ozziel Rueda Diaz <ozzielrueda@gmail.com>
2023-01-11 14:03:51 -0600

    Ejemplo del condicional if en C#

--- Condicional_if/Program.cs
+++ Condicional_if/Program.cs
@@ -2,9 +2,27 @@
 {
     private static void Main(string[] args)
     {
+        Console.WriteLine("Ejemplo de variable boolean");
         bool haceFrio;
         haceFrio = false;
 
         Console.WriteLine(!haceFrio);
+
+        Console.WriteLine("Ejemplo de condicional if");
+
+        int edad = 0;
+        Console.WriteLine("Vamos a evaluar si eres mayor de edad");
+        edad = int.Parse(Console.ReadLine());
+
+        if (edad >= 18)
+        {
+            Console.WriteLine("Eres mayor de edad, puedes pasar.");
+        }
+        else
+        {
+            Console.WriteLine("No tienes la edad permitida para entrar");
+        }
+
+
     }
 }
