b8bd01c6aa30937e1f3417b555f9485e0e03db86
Ozziel Rueda Diaz <ozzielrueda@gmail.com>
2023-01-12 17:46:19 -0600

    Ejemplo de condicional if con operadores logicos y sus particularidades

--- Condicional_if/Program.cs
+++ Condicional_if/Program.cs
@@ -23,6 +23,25 @@
             Console.WriteLine("No tienes la edad permitida para entrar");
         }
 
+        Console.WriteLine("Ejemplo del condicional IF con operadores lógicos");
+        string nombre = "";
+        bool carnet= false;
+        string aux = "";
+
+        Console.WriteLine("Escriba su nombre porfavor");
+        nombre = Console.ReadLine();
+        Console.WriteLine("Escriba su edad porfavor");
+        edad = int.Parse(Console.ReadLine());
+        Console.WriteLine("Tienes carnet (si/no)");
+        aux = Console.ReadLine();
+        if (aux == "si") carnet = true;
+        if (edad >= 18 && carnet)  Console.WriteLine($"{nombre} su edad de {edad} y teniendo carnet, tiene derecho a conducir el vehiculo");
+        else  Console.WriteLine($"{nombre} su edad de {edad} y al no tener carnet, NO tiene derecho a conducir el vehiculo");
+
+
+
+
+
 
     }
 }
